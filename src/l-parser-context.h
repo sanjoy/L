5f2e8e0b48b3cf1a7bb0b4beda719b0c16d419d9
Sanjoy Das <sanjoy@playingwithpointers.com>
2010-10-02 15:55:33 +0530

    Minor changes.
    	modified:   l-parser-context.h
    	new file:   l-structures.h

--- src/l-parser-context.h
+++ src/l-parser-context.h
@@ -4,10 +4,17 @@
 #include "l-structures.h"
 #include "l-mempool.h"
 
-struct _LParserContext {
+#include <stdio.h>
+
+typedef struct {
+
 	void *scanner_data;
 	LMempool *mempool;
 	LUniversalNode *roots;
-};
+
+	char *input_string;
+	FILE *input_file;
+
+} LParserContext;
 
 #endif /* __L__PARSER__CONTEXT__H */
---
+++ src/l-structures.h
@@ -0,0 +1,71 @@
+#ifndef __STRUCTURES__H
+#define __STRUCTURES__H
+
+#include "l-mempool.h"
+
+typedef struct {
+	char *name;
+	int idx;
+} LToken;
+
+typedef struct _LLambda LLambda;
+
+struct _LTreeNode {
+	LToken *token;
+	LLambda *lambda;
+	struct _LTreeNode *right_sibling, *first_child;
+};
+
+typedef struct _LTreeNode LTreeNode;
+
+struct _LListNode {
+	LToken *token;
+	struct _LListNode *next;
+};
+
+typedef struct _LListNode LListNode;
+
+struct _LLambda {
+	LListNode *args;
+	LTreeNode *body;
+};
+
+typedef struct {
+	LToken *lhs;
+	LLambda *rhs;
+} LAssignment;
+
+typedef enum {
+	NODE_ASSIGNMENT,
+	NODE_LAMBDA
+} LUniversalNodeType;
+
+struct _LUniversalNode {
+
+	LUniversalNodeType type;
+
+	union {
+		LAssignment *assignment;
+		LLambda *lambda;
+	};
+	
+	struct _LUniversalNode *next;
+
+};
+
+typedef struct _LUniversalNode LUniversalNode;
+
+LToken *l_token_new (LMempool *, char *);
+
+LTreeNode *l_tree_cons_horizontal (LMempool *, LToken *, LTreeNode *);
+LTreeNode *l_tree_cons_vertical (LMempool *, LTreeNode *, LTreeNode *);
+LTreeNode *l_tree_cons_lambda (LMempool *, LLambda *, LTreeNode *);
+
+LListNode *l_list_cons (LMempool *, LToken *, LListNode *);
+
+LLambda *l_lambda_new (LMempool *, LListNode *, LTreeNode *);
+LAssignment *l_assignment_new (LMempool *, LToken *, LLambda *);
+
+void l_register_universal_node (LMempool *, LUniversalNodeType, void *, void *);
+
+#endif /* __STRUCTURES__H */
